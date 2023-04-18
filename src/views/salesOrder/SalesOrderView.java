b33c59a343d2933a391f03b97e206a59f34fa78c
alexcraig481 <acfrady@my.waketech.edu>
2023-04-18 15:08:59 -0400

    Added commits

--- src/views/salesOrder/SalesOrderView.java
+++ src/views/salesOrder/SalesOrderView.java
@@ -11,5 +11,33 @@ import javafx.scene.layout.VBox;
  * @author AlexC
  */
 public class SalesOrderView extends VBox{
+    //References for SalesOrderForm salesOrderForm, SalesOrderTable, salesOrderTable
+    
+    //References for Button processSaleBtn and Button cancelBtn
+    
+    
+    //Constructor
+    
+        //Title
+    
+        //Create HBox for Buttons
+    
+        //Add buttons to HBox
+    
+        //Style buttons (prefWidth is 150)
+    
+        //HBox styling (spacing 20 and alignment center)
+    
+        //Create new SalesOrderTable for Reference
+    
+        //Create new SalesOrderForm for Reference
+    
+        //Add to this pane title, salesOrderTable, salesOrderForm, buttonPane
+    
+    
+    //Getters for the form, table, and buttons
+    
+    
+    
     
 }
