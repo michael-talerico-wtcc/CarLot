562af5a347ed910724b86d4f55ec8dfd17a9a227
alexcraig481 <acfrady@my.waketech.edu>
2023-04-18 15:01:27 -0400

    Changed commits

--- src/views/purchaseOrder/PurchaseOrderForm.java
+++ src/views/purchaseOrder/PurchaseOrderForm.java
@@ -27,8 +27,9 @@ public class PurchaseOrderForm extends HBox{
         
         
         // Add Labels and Textfields to Grid with add(Node, col index, row index)
-       
-        
+            //Row 0: datePurchased label/datepicker, cost label/textfield 
+            //Row 1: vin label/textfield, year label/textfield, make label/textfield, model label/textfield
+            //Row 2: color label/textfield, mileage label/textfield, mpg label/textfield, salesPrice label/textfield 
         
         
         
@@ -37,7 +38,7 @@ public class PurchaseOrderForm extends HBox{
         
         
       
-        //Set PurchaseOrderForm Styes
+        //Set PurchaseOrderForm Styles (padding at 20, create border)
    
     }
     
