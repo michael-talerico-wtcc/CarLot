5ac614cbcc54e8d5578e26f5edd949cfc0bf87ec
alexcraig481 <acfrady@my.waketech.edu>
2023-04-18 14:50:47 -0400

    Added Comments

--- src/views/purchaseOrder/PurchaseOrderView.java
+++ src/views/purchaseOrder/PurchaseOrderView.java
@@ -11,5 +11,32 @@ import javafx.scene.layout.VBox;
  * @author AlexC
  */
 public class PurchaseOrderView extends VBox{
+    //Attributes PurchaseOrderTable purchaseOrderTable, PurchaseOrderForm purchaseOrderForm,
+        //TextField salesPrice, Button addToInventoryBtn, Button cancelBtn
+    
+    //Constructor
+    
+        //Title
+    
+        //Create HBox for CarLot SalesPrice TextField and the two Buttons
+    
+        //Add Label, salesPrice TextField, addToInventoryBtn, and cancelBtn to the
+        // HBox
+    
+        //Button Styles (PrefWidth - 150)
+    
+    
+        //HBox Style (Spacing is 20)
+    
+        //Create PurchaseOrderTable for the Attribute Reference
+    
+        //Create PurchaseOrderForm for the Attribute Reference
+    
+        //Set Styles for This Pane
+    
+        //Add title, table, form, and salesPrice/ButtonPane
+    
+    
+    //Create Getters for Table, Form, Buttons, and TextField
     
 }
