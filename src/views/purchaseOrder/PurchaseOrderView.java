ba7dc9d571511f3943c79f33e3672138033e96d9
alexcraig481 <acfrady@my.waketech.edu>
2023-04-20 15:57:39 -0400

    Added new Hbox for error message Text

--- src/views/purchaseOrder/PurchaseOrderView.java
+++ src/views/purchaseOrder/PurchaseOrderView.java
@@ -11,12 +11,32 @@ import javafx.scene.layout.VBox;
  * @author AlexC
  */
 public class PurchaseOrderView extends VBox{
-    //References PurchaseOrderTable purchaseOrderTable, PurchaseOrderForm purchaseOrderForm,
-        //TextField salesPrice, Button addToInventoryBtn, Button cancelBtn
+    //References for PurchaseOrderTable purchaseOrderTable, PurchaseOrderForm purchaseOrderForm
+    
+     //References for TextField salesPrice
+    
+    //References for Buttons add ToInventoryBtn, Button cancelBtn
+    
+    //Reference for error message text: Text errorMessage
+    
     
     //Constructor
     
-        //Title
+        //Create Title
+    
+        //Create PurchaseOrderTable for the Attribute Reference
+    
+        //Create PurchaseOrderForm for the Attribute Reference
+    
+        //Create HBox for error message Text
+    
+        //Set error message HBox Styles (alignment - center)
+    
+        //Create Text object for error message
+    
+        //Set Text styles(Font(Arial,BOLD, 13), Fill(Color is red), Stroke(Color is red))
+    
+        //Add Text to error message HBox
     
         //Create HBox for CarLot SalesPrice TextField and the two Buttons
     
@@ -28,15 +48,12 @@ public class PurchaseOrderView extends VBox{
     
         //HBox Style (Spacing is 20)
     
-        //Create PurchaseOrderTable for the Attribute Reference
-    
-        //Create PurchaseOrderForm for the Attribute Reference
-    
+       
         //Set Styles for This Pane
     
         //Add title, table, form, and salesPrice/ButtonPane
     
     
-    //Create Getters for Table, Form, Buttons, and TextField
+    //Create Getters for Table, Form, Buttons, and TextField, Text
     
 }
