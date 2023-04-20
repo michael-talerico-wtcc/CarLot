1b57e1c5de87855fc1d54e13942c3a857c09443c
Nate-is-Great <114320605+Nate-is-Great@users.noreply.github.com>
2023-04-20 12:13:19 -0400

    Completed SalesOrder Please Contact me if there are any errors thank you Completed in (Git Hub) IDE

--- src/models/SalesOrder.java
+++ src/models/SalesOrder.java
@@ -9,15 +9,54 @@ import java.time.LocalDate;
 /**
  *
  * @author 
- */
+ *// // Nate completed SalesOrder please contact me if there is error ///ncleach@my.waketech.edu///
 public class SalesOrder {
     //Attributes int id, LocalDate dateSold, Car car, double priceSold
-    
+    private int id;
+    private LocalDate dateSold;
+    private Car car;
+    private double priceSold;
+
     //Constructor
+    SalesOrder(){
+
+    }
+    SalesOrder(int id, LocalDate dateSold, Car car, double priceSold){
+    this.id = id;
+    this.dateSold = dateSold;
+    this.car = car;
+    this.priceSold = priceSold;
+
+    }
     
-    //Getters and Setters
+    //Getters
+    public int getId(){
+        return id;
+    }
+    public LocalDate getDateSold(){
+        return dateSold;
+    }
+    public Car getCar(){
+        return car;
+    }
+    public double getPriceSold(){
+        return priceSold;
+    }
+    //Setters
+    public void setId(int id){
+        this.id = id;
+    }
     
+    public void setCar(Car car){
+        this.car = car;
+    }
+    public void setPriceSold(double priceSold){
+        this.priceSold = priceSold;
+    }
+
     //ToString
-    
+    public String ToSring(int id, LocalDate dateSold, Car car, double priceSold){
+        return "Id " + id + " DateSold " + dateSold + " Car " + car + " PriceSold " + priceSold;
+    }
     
 }
